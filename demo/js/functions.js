6cb965eaa3ec23e84cd118c297a40b2124300e2a
Varin6 <varinator@gmail.com>
2017-08-01 23:36:41 +0100

    Comment

--- demo/js/functions.js
+++ demo/js/functions.js
@@ -3,15 +3,22 @@
  */
 
 $(function () {
+    setTimeout(helperInit, 100);
+});
+
+
+function helperInit () {
     $('.hbtn').not($('.hbtn-x')).on('click', function (e) {
         e.preventDefault();
         var outer = $(this).prop('outerHTML');
         console.log(outer);
         klon = $(this).clone().css('margin-right', '20px');
-        $('.class-helper').show().css('display', 'flex').empty().prepend('<pre></pre>').prepend(klon);
-        $('.class-helper pre').text(outer);
+
+        $('.class-helper .class-helper__inner').css('display', 'flex').empty().prepend('<pre></pre>').prepend(klon);
+        $('.class-helper .class-helper__inner pre').text(outer);
+        $('.class-helper').slideDown();
     });
-});
+}
 
 
 $(function () {
@@ -19,9 +26,32 @@ $(function () {
         e.preventDefault();
         $('.hbtn').not($('.hbtn-x')).toggleClass('hpill');
         $(this).toggleClass('hpill');
-        $('.class-helper').hide();
+        $('.class-helper').slideUp();
         $(this).text(function(i, text){
             return text === "SWITCH TO SOFT!" ? "SWITCH TO SHARP!" : "SWITCH TO SOFT!";
         });
     });
+});
+
+$(function () {
+    $('.close-icon').on('click', function () {
+        $('.class-helper').slideUp();
+    });
+});
+
+
+
+
+$(function () {
+
+});
+
+
+
+
+
+
+
+$('.hbtn').not($('.hbtn-x')).on('click', function (e) {
+
 });
