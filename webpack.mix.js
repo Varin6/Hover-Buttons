4bae1c3fde6a4835c2e229eee330c5366a8d86be
Varin6 <varinator@gmail.com>
2017-08-05 01:33:26 +0100

    Webpack + package.json

---
+++ webpack.mix.js
@@ -0,0 +1,26 @@
+let mix              = require('laravel-mix');
+let LiveReloadPlugin = require('webpack-livereload-plugin');
+
+mix
+    .setPublicPath('../Hover-Buttons')
+    .setResourceRoot('../Hover-Buttons')
+    .options(
+        {
+            processCssUrls: false,
+            postCss: [
+                require('autoprefixer')({
+                    browsers: ['last 5 versions'],
+                    cascade: false
+                })
+            ]
+        })
+    .sass('scss/hoverbuttons.scss', 'css/hoverbuttons.css')
+    .sass('demo/css/style.scss', 'demo/css/style.css')
+    .combine(
+        [
+            'css/hoverbuttons.css',
+            'demo/css/style.css'
+        ], 'demo/css/core.css'
+    )
+
+;
